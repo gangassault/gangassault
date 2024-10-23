305c81fae778c5cd3d818b8a28127b462852b6b7
gangassault <gangassault@gmail.com>
2024-10-23 18:14:42 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "phpcrud");
+$con=mysqli_connect("my-db.cjksus4ey352.ap-south-1.rds.amazonaws.com", "admin", "gangassault", "phpcrud");
 if(mysqli_connect_errno())
 {
 echo "Connection Fail".mysqli_connect_error();
